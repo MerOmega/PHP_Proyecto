b88a9bb18e7c08c3b7d1b1fbc0b7618c32691314
MerOmega <32043630+MerOmega@users.noreply.github.com>
2021-05-03 18:41:59 -0300

    Cambio del XP

--- validar.js
+++ validar.js
@@ -1,6 +1,5 @@
-var nombre = document.forms[0].querySelector('input[name="nombre"]');
-var apellido = document.forms[0].querySelector('input[name="apellido"]');
-
+var nombre = document.querySelector('input[name="nombre"]');
+var apellido = document.querySelector('input[name="apellido"]');
 function valid(event){
     if(nombre.value===""){
         alert("El nombre no puede estar vacio");
