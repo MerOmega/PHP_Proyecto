ebe9825d85c531c3d02267268de9690124956140
MerOmega <32043630+MerOmega@users.noreply.github.com>
2021-05-08 16:27:51 -0300

    agregada paginacion.php y producto

--- index.php
+++ index.php
@@ -114,100 +114,7 @@ function procesoFiltroCategory($var1){
 
      <div>
          <!-- Consulta a la base-->          
-                <?php 
-                if(isset($_POST['sort'])){
-                    $_SESSION['sort_user']=$_POST['sort']; 
-                    procesoFiltroSort();
-                }
-                if(isset($_POST['categorias'])){
-                    $_SESSION['category_user']=$_POST['categorias'];
-                    procesoFiltroCategory($_POST['categorias']);
-                }
-
-                if(isset($_SESSION['category_user'])){
-                        $categoria=$_SESSION['category_user'];
-                        $string=$_SESSION['sort_user_defined'];
-                        $sql= "SELECT COUNT(*)FROM productos P INNER JOIN categorias_productos C ON 
-                        (P.idCategoriaProducto = C.idCategoriaProducto) $categoria $string";  
-                }else{
-                $string=$_SESSION['sort_user_defined'];
-                $sql= "SELECT COUNT(*) FROM productos WHERE (idUsuarioComprador<=>NULL) $string";       
-                }
-                
-
-                //obtengo la cantidad total de elementos 
-                $result = $conn->query($sql);
-                $row = mysqli_fetch_row($result);
-               
-                $rows = $row[0];
-                echo "<br>Cantidad de productos: ".$rows;
-                //Cantidad de elementos a mostrar
-                $page_rows = 5;
-                //Saco la cuenta de cuantas paginas voy a mostrar, ceil redondea
-                $last = ceil($rows/$page_rows);
-                //Tengo solamente una pagina para mostrar
-                if($last<1){
-                    $last=1;
-                }
-                //Arranca en la pagina 1
-                $pagenum =1;
-                //Filtro para que no escriban algo distinto de numeros
-                if(isset($_GET['pn'])){
-                    $pagenum=preg_replace('#[^0-9]#','',$_GET['pn']);
-                }
-                //Controla que no se salga de rango
-                if($pagenum<1){
-                    $pagenum=1;
-                }else if($pagenum>$last){
-                    $pagenum=$last;
-                }
-                //Saltea y muestra los proximos 5
-                $saltea=($pagenum-1)*$page_rows;
-
-                $limit = 'LIMIT ' .$saltea.','.$page_rows;
-
-                if(isset($_SESSION['category_user'])){
-                        $categoria=$_SESSION['category_user'];
-                        $string=$_SESSION['sort_user_defined'];
-                        $sql= "SELECT P.*FROM productos P INNER JOIN categorias_productos C ON 
-                        (P.idCategoriaProducto = C.idCategoriaProducto) $categoria $string $limit";           
-                }else{
-                    $string= $_SESSION['sort_user_defined'];
-                    $sql= "SELECT * FROM productos WHERE (idUsuarioComprador<=>NULL) $string $limit";
-                }
-                
-                $result = $conn->query($sql);
-                $texto="Pagina <b>$pagenum</b> of <b>$last</b>";
-                $paginCtrls='';
-                //mas de 1 pagina
-                if($last!=1){
-                    if($pagenum>1){
-                        $previo = $pagenum-1;
-                        $paginCtrls .='<a href="'.$_SERVER['PHP_SELF'].'?pn='.$previo.'">Previo</a>';
-                        
-                        for($i=$pagenum-2;$i<$pagenum;$i++){
-                            if($i>0){
-                                $paginCtrls .='<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a>';
-                        
-                            }
-                        }
-
-                    }
-                    $paginCtrls.='<p>'.$pagenum.'</p>';
-                    for($i=$pagenum+1;$i<=$last;$i++){
-                        $paginCtrls .='<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'">'.$i.'</a>';
-                            if($i>=$pagenum+2){
-                                break;
-                            }
-                    }
-                    if($pagenum!=$last){
-                        $next =$pagenum+1;
-                         $paginCtrls .='<a href="'.$_SERVER['PHP_SELF'].'?pn='.$next.'">Siguiente</a>';
-
-                    }
-                }
-                
-?>
+               <?php require('paginacion.php');?>
 
     </div> 
 
@@ -219,7 +126,7 @@ function procesoFiltroCategory($var1){
                    ?>
                     <div class="blackbox">
                     <?php
-                            echo '<img src="data:image;base64,'.base64_encode($row["contenidoimagen"]).'" alt="Image" style="width="100px; height=150" >';
+                            echo '<a href="producto.php?id='.$row["idProducto"].'"><img src="data:image;base64,'.base64_encode($row["contenidoimagen"]).'" alt="Image" style="width="100px; height=150" ></a>';
                             ?>
                             <p><?php echo "Articulo: " . $row["nombre"] . " ID: ". $row['idProducto']?></p>
                             <div class="caduca"><p><?php echo $row["caducidad"] ?></p></div>
---
+++ producto.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+
+<?php
+    session_start();
+?>
+
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+<head>
+    <title>
+        Compra Barato
+    </title>
+    <link rel="stylesheet" type="text/css" href="main.css?v=1.1">
+    <meta name="viewport" content="width=device-width">
+</head>
+<body>
+<header>
+<div class="cabecera">
+         <h1>CompraBarato!</h1>
+        <div class="buscador">
+   
+            <form > 
+                <input type="search" id="query" name="b" placeholder="Busca aqui..."> 
+                <button>Search</button>
+            </form>
+        </div>
+  </div>
+    <div class="main-nav">
+        <nav>
+                
+                <div id="interfaz">
+                    <a href="index.php">Inicio</a>
+                </div>
+                <div id="usuarios">
+                    <a href="login.php">Iniciar Sesion</a>
+                    <a href="registrar.php">Crear Cuenta</a>
+                    <a href="logout.php">Log Out</a>
+                </div>
+        </nav>
+    </div>
+</header>
+<?php
+
+    if(isset($_GET['id'])){
+        echo $_GET['id'];
+    }else{
+        echo "No encontre el ID";
+    }
+
+?>
+
+
+    <script type="text/javascript">  
+        var sesionactual='<?php echo $_SESSION['nombredeusuario'] ?>'; 
+        function cambioUsuario(){
+            if(sesionactual!="Invitado"){
+                $("#usuarios").replaceWith( '<div id="usuarios"> <a href="#">Hola <?php echo $_SESSION['nombredeusuario']; ?></a> <a href="logout.php">Log Out</a> </div>');
+            }
+        }
+        cambioUsuario(sesionactual);
+    </script>
+</main>
+</body>
+
+</html>
