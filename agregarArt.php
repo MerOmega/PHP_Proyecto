4bb2069a625c857432b9297650e6c0003a5b2b4d
MerOmega <32043630+MerOmega@users.noreply.github.com>
2021-06-11 21:41:39 -0300

    Agregado pag agregar productos

--- agregarArt.php
+++ agregarArt.php
@@ -2,7 +2,7 @@
 <!doctype html>
 <html>
 <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-<script defer src="popup.js"></script>
+
 <?php
     session_start(); 
 ?>
@@ -11,6 +11,7 @@
         Compra Barato - Perfil Usuario
     </title>
     <link rel="stylesheet" type="text/css" href="main.css?v=1.1">
+    <link rel="stylesheet" type="text/css" href="agregart.css?v=1.1">
     <meta name="viewport" content="width=device-width">
 </head>
 <body>
@@ -46,7 +47,36 @@
     require("BD.php");
     ?>
     <h2>Agregue su producto!</h2>
-    
+    <form id="addprod" style="text-align:center;" name="registro" action="agregarArt.php" method="POST" novalidate>
+        <label>Titulo de su publicacion</label><br>
+        
+        <input type="text" name="titulo" placeholder="Ingrese titulo del producto"><br><br>
+
+        <label>Seleccione categoria</label><br>
+        <div id="sorted">
+                <?php 
+                //consulta sql para obtener todas las categorias de productos
+                $sql= "SELECT * FROM categorias_productos";?>
+                </div>
+                <?php
+                //busqueda en la bd de la consulta sql
+                $result = $conn->query($sql);
+                ?>
+    </div> 
+        <!--Fomulario seleccionador para elegir categorias-->
+        <div id="contenedor_categorias">
+                            <select name="categorias">
+                            <?php while($datos=mysqli_fetch_array( $result )){ ?>
+                            <option value="<?php echo $datos['nombre'] ?>"><?php echo $datos['nombre'] ?></option>
+                            <?php } ?>
+                            </select>
+        </div> <br>
+        <label>Descripcion del producto</label><br>
+        <textarea type="text" rows="5" cols="60" name="descripcion" placeholder="Ingrese la descripcion" ></textarea><br><br>
+        <input type="file" name="file"><br><br>
+        <button type="submit" name="upload">Agregar!</button>
+
+    </form>
     
 <?php 
  require("userbanner.php");
---
+++ registro.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <title>
+        Compra Barato
+    </title>
+    <link rel="stylesheet" type="text/css" href="main.css?v=1.1">
+    <meta name="viewport" content="width=device-width">
+</head>
+<body>
+</body>
+<a href="index.php">Inicio</a>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia corporis ut voluptatum? Sequi officia accusamus, perspiciatis ipsam necessitatibus et. Libero velit debitis eos repellat nobis neque earum voluptatem dolor tempora!</p>
+</html>
